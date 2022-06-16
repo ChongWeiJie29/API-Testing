8be9776cb109c00083f7f8a90fd0ad1b7927552c
WeiJie2909 <wei.cho@handshakes.com.sg>
2022-06-16 14:07:00 +0800

    test24

--- src/startServer.ts
+++ src/startServer.ts
@@ -6,15 +6,9 @@ dotenv.config();
 
 app.get('/', (req:any, res:any) => {
     console.log("Request received.");
-    res.send({"answer":3});
+    res.send({"answer":4});
     console.log("Response sent");
 })
-
-app.get("/second", (req:any, res:any) => {
-    console.log("Request received.");
-    res.send({"answer":3});
-    console.log("Response sent");
-  })
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
